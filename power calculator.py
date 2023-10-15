8327b73a6ff448610df95c1498b062a8d33938b6
Carl Baines <carltbaines@hotmail.co.uk>
2023-10-15 11:25:53 +0100

    Delete power calculator.py

--- power calculator.py
+++
@@ -1,9 +0,0 @@
-def powerCalc():
-    voltage = int(input("Please enter the total voltage of the appliance ")) #inputs.
-    current = int(input("Please enter the total current of the appliance "))
-
-    pwr = (voltage*current) #power equation
-
-    print(str(pwr) + 'W') #adds Watts unit on the end.
-
-powerCalc()
