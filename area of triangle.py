b42d54de7a91398a568a890ae25f91d3f592be3f
Carl Baines <carltbaines@hotmail.co.uk>
2023-10-15 11:25:43 +0100

    Delete area of triangle.py

--- area of triangle.py
+++
@@ -1,8 +0,0 @@
-def triArea():
-    #inputs
-    base = int(input('Please enter the base of the triangle '))
-    height = int(input('Please enter the height of the triangle '))
-    #calculation
-    a = (base*height/2)
-    print(a)
-triArea()
